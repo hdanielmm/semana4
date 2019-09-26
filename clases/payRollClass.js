9c3998954a5c7dbd1214edadd2f636ccd15bbb9c
daniel <hdanielmm@gmail.com>
2019-09-26 16:52:11 +0200

    add some modifications

--- clases/payRollClass.js
+++ clases/payRollClass.js
@@ -1,41 +1,41 @@
 class Employee {
   constructor(name, salary) {
-    this.name = name
-    this.baseSalary = salary
+    this.name = name;
+    this.baseSalary = salary;
   }
-  
+
   getDeductionHealth() {
-    return this.baseSalary * 0.04
+    return this.baseSalary * 0.04;
   }
 
   getDeductionRetirement() {
-    return this.baseSalary * 0.08
+    return this.baseSalary * 0.08;
   }
-  
+
   getSalary() {
     const deductionHealth = getDeductionHealth();
     const deductionRetirement = getDeductionRetirement();
-    return this.baseSalary - deductionHealth - deductionRetirement
+    return this.baseSalary - deductionHealth - deductionRetirement;
   }
 
 }
 
 class Manager extends Employee {
   constructor(name, salary, bonus) {
-    super(name, salary)
-    this.bonus = bonus
+    super(name, salary);
+    this.bonus = bonus;
   }
 
   getSalary() {
-    return super.getSalary() + this.bonus
+    return super.getSalary() + this.bonus;
   }
 }
 
 class Company {
   constructor() {
-    this.employees = []
-    this.healthFund = 0
-    this.retirementFund = 0
+    this.employees = [];
+    this.healthFund = 0;
+    this.retirementFund = 0;
   }
 
   addEmployees(...empls) {
@@ -43,25 +43,25 @@ class Company {
   }
 
   generatePayroll() {
-    let payroll = 0
+    let payroll = 0;
     for (let i = 0; i < this.employees.length; i++) {
       const employee = this.employees[i];
       payroll += employee.getSalary();
       this.healthFund += employee.getDeductionHealth();
       this.retirementFund += employee.getDeductionRetirement();
     }
-    return payroll
+    return payroll;
   }
 }
 
 
-const e1 = new Employee('Pedro', 2300)
-const e2 = new Employee('Pablo', 3500)
-const m1 = new Manager('Maria', 5000, 2500)
+const e1 = new Employee('Pedro', 2300);
+const e2 = new Employee('Pablo', 3500);
+const m1 = new Manager('Maria', 5000, 2500);
 
-e1.getSalary()
-e2.getSalary()
-// m1.getSalaryManager()
+e1.getSalary();
+e2.getSalary();
+m1.getSalary();
 
 const company = new Company();
 company.addEmployees(e1, e2, m1);
