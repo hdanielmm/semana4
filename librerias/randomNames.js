f1316215da1d52072a8ba4845e56baf219dc8596
daniel <hdanielmm@gmail.com>
2019-09-27 03:01:32 +0200

    add libreria's folder

---
+++ librerias/randomNames.js
@@ -0,0 +1,13 @@
+const readlineSync = require('readline-sync');
+const faker = require('faker');
+const fs = require('fs');
+
+const numberNames = readlineSync.question('\nHow many names would you like to generate? ');
+const fileName = readlineSync.question('Please enter a file name: ');
+
+
+for (let i = 0; i < numberNames; i++) {
+  fs.writeFileSync(fileName + '.txt', faker.name.firstName() + '\n', { flag: 'a' });
+}
+console.log('File text ' + fileName + '.txt was generated with ' + numberNames + ' names!\n');
+
