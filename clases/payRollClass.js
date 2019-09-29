e940c7d4437351d513202d57a47a71aaed2b2c2c
daniel <hdanielmm@gmail.com>
2019-09-30 01:05:03 +0200

    add finished payRollClass.js

--- clases/payRollClass.js
+++ clases/payRollClass.js
@@ -1,3 +1,5 @@
+// PayRoll Calculator
+
 class Employee {
   constructor(name, salary) {
     this.name = name;
@@ -13,8 +15,8 @@ class Employee {
   }
 
   getSalary() {
-    const deductionHealth = getDeductionHealth();
-    const deductionRetirement = getDeductionRetirement();
+    const deductionHealth = this.baseSalary * 0.04; // si uso la función getDeductionHealth() sale error diciendo que no está declarada
+    const deductionRetirement = this.baseSalary * 0.08; // el mismo problema de la línea anterior
     return this.baseSalary - deductionHealth - deductionRetirement;
   }
 
@@ -54,7 +56,6 @@ class Company {
   }
 }
 
-
 const e1 = new Employee('Pedro', 2300);
 const e2 = new Employee('Pablo', 3500);
 const m1 = new Manager('Maria', 5000, 2500);
@@ -65,81 +66,13 @@ m1.getSalary();
 
 const company = new Company();
 company.addEmployees(e1, e2, m1);
-company.generatePayroll(); // 2024 + 3080 + 6900 = 12004
+console.log(company.generatePayroll()); // 2024 + 3080 + 6900 = 12004
 
-/******************************************************************************************/
-// Propio
-/*
-class Employee {
-  constructor(name, salary) {
-    this.name = name
-    this.salary = salary
-    this.deductionHealth = 0
-    this.deductionRetirement = 0
-  }
-  getSalary() {
-    this.deductionHealth = this.salary * 0.04
-    this.deductionRetirement = this.salary * 0.08
-    this.salary = this.salary - this.deductionHealth - this.deductionRetirement
-    return this.salary
-  }
-}
-class Manager extends Employee {
-  constructor(name, salary, bonus) {
-    super(name, salary)
-    this.bonus = bonus
-  }
-  getSalaryManager() {
-    this.salary = this.getSalary() + this.bonus
-    return this.salary
-  }
-}
 
-class Company {
-  constructor() {
-    this.p = []
-    this.totalHealthFund = 0
-    this.totalRetirementFund = 0
-  }
-  addEmployees(...empl) {
-    for (let i = 0; i < empl.length; i++) {
-      this.p[i] = empl[i]
-    }
-  }
-  generatePayroll() {
-    this.payroll = 0
-    for (let i in this.p) {
-      this.payroll += this.p[i].salary
-    }
-    return this.payroll
-  }
-  healthFund() {
-    for (let i in this.p) {
-      this.totalHealthFund += this.p[i].deductionHealth
-    }
-    return this.totalHealthFund
-  }
-  retirementFund() {
-    for (let i in this.p) {
-      this.totalRetirementFund += this.p[i].deductionRetirement
-    }
-    return this.totalRetirementFund
-  }
-}
+console.log(company.healthFund); // 432
+console.log(company.retirementFund); // 864
 
-const e1 = new Employee('Pedro', 2300)
-const e2 = new Employee('Pablo', 3500)
-const m1 = new Manager('Maria', 5000, 2500)
+console.log(company.generatePayroll()); // 12004
+console.log(company.healthFund); //864
+console.log(company.retirementFund); // 1728
 
-e1.getSalary()
-e2.getSalary()
-m1.getSalaryManager()
-
-const company = new Company()
-company.addEmployees(e1, e2, m1)
-console.log(company.generatePayroll())
-console.log(company.healthFund())
-console.log(company.retirementFund())
-console.log(company.generatePayroll())
-console.log(company.healthFund())
-console.log(company.retirementFund())*/
